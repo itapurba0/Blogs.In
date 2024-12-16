90384ea32bd508f4e39d126523281f491c564846
itapurba0 <apurba00237@gmail.com>
2024-12-16 17:13:07 +0530

    Update AppBar.tsx

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Avatar } from "./Avatar";
 import { useUser } from "../hooks/Index";
-
+import { HiOutlineLogout } from "react-icons/hi";
 export const AppBar = () => {
-  const { loading, user } = useUser();
+ 
+ const { loading, user} = useUser()
+ const navigate = useNavigate();
+  const logout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
 
   if (loading) {
     return (
@@ -13,11 +19,10 @@ export const AppBar = () => {
     );
   }
 
-  const authorname = user?.name || "Anonymous";
+  const authorname = user?.name.split(" ")[0] || "Anonymous";
 
   return (
     <div className="fixed top-0 left-0 w-full bg-[#6A1B9A] border-b-[1px] border-[#5A157F] shadow-md px-6 py-4 flex items-center justify-between z-50">
-      {/* Left Section */}
       <div className="flex items-center space-x-3">
         <div className="h-10 w-10 bg-[#5A157F] rounded-full shadow-md"></div>
         <Link
@@ -28,18 +33,26 @@ export const AppBar = () => {
         </Link>
       </div>
 
-      {/* Right Section */}
       <div className="flex items-center space-x-6">
-        <Link to="/Publish">
-          <button
-            className="bg-[#8B6EA9] text-white px-5 py-2.5 rounded-full shadow-md hover:bg-[#6E4A8B] hover:shadow-lg transition-all duration-300"
-          >
-            New
-          </button>
-        </Link>
-        <Link to="/MyBlogs">
-          <Avatar size={10} name={authorname} />
-        </Link>
+       
+          <>
+            <Link to="/Publish">
+              <button
+                className="bg-[#8B6EA9] text-white px-5 py-2.5 rounded-full shadow-md hover:bg-[#6E4A8B] hover:shadow-lg transition-all duration-300"
+              >
+                New
+              </button>
+            </Link>
+            <div className="flex items-center space-x-2">
+              <Link to="/myblogs"  className="flex items-center space-x-1">
+              <Avatar size={10} name={authorname} />
+              <span className="text-white">{authorname}</span>
+              </Link>
+              <div onClick={logout}>
+                <HiOutlineLogout size={25} className="text-white cursor-pointer" color="white" />
+              </div>
+            </div>
+          </>
       </div>
     </div>
   );
