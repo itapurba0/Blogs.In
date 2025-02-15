a110e9a24d3f96e8c48888155e5c129744703d18
Apurba Roy <apurba00237@gmail.com>
2025-02-15 22:20:29 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors';
 import { userRouter } from './routes/user';
 import { blogRouter } from './routes/blog';
+import { commentRouter } from './routes/comments';
 
 const app = new Hono<{
 	Bindings: {
@@ -12,5 +13,5 @@ const app = new Hono<{
 app.use('/*',cors());
 app.route('/api/v1/user', userRouter);
 app.route('/api/v1/blog', blogRouter);
-
+app.route('/api/v1/comments', commentRouter);
 export default app
