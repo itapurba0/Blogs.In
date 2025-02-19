9ec5d9a04e28df49a783da5f5bb110987dfdacab
Apurba Roy <apurba00237@gmail.com>
2025-02-19 23:15:43 +0530

    Update Blog.tsx

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,4 +1,4 @@
-import {  FullBlog} from "../components/export";
+import { FullBlog } from "../components/export";
 import { useBlog } from "../hooks/Index";
 import { useParams } from "react-router-dom";
 export const Blog = () => {
